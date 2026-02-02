ccce25892b0644f4377090b6927c35e49bafa5bf
Aditya Kartik <adityakartik43@gmail.com>
2026-02-02 21:34:22 +0530

    TTN Assignments

---
+++ JS and Typescript Assignments/Monday/Q1/script.js
@@ -0,0 +1,6 @@
+var p = prompt("Enter Principal amount");
+var r = prompt("Enter rate of interest");
+var t = prompt("Enter year");
+
+var si = (p * r * t) / 100;
+console.log(si)
---
+++ JS and Typescript Assignments/Monday/Q3/script.js
@@ -0,0 +1,5 @@
+var r = prompt("Enter radius")
+
+var ans = Math.PI * r * r;
+
+console.log(ans)
